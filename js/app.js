f54449c9626633137c9fd74240c3c97aae8e4137
LucasCtrl <lucasalt@protonmail.com>
2020-10-19 20:39:35 +0200

    :sparkles: Add search box feature

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const glyphsContainer = document.getElementById('glyphsContainer')
+const searchBox = document.getElementById('searchBox')
 
 async function loadGlyphs() {
   try {
@@ -6,6 +7,18 @@ async function loadGlyphs() {
     const glyphs = await res.json()
     console.log(glyphs)
     renderGlyph(glyphs.glyphs)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function searchBoxChange(value) {
+  try {
+    const res = await fetch('../data/glyphs.json')
+    const glyphs = await res.json()
+    const filteredGlyphs = glyphs.glyphs.filter((g) => g.name.toLowerCase().includes(value.toLowerCase()))
+    console.log(filteredGlyphs)
+    renderGlyph(filteredGlyphs)
   } catch (err) {
     console.error(err)
   }
